e0a8d82a19e799818c42a46a09373b4cabb05ae2
NL <Lapina.n.b@mail.ru>
2022-01-05 00:25:32 +1000

    плогиат

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './word/data';
+import { isEditable } from '@testing-library/user-event/dist/utils';
+
+
+function Heder() {
+  return (
+  <>
+  <a href='#'>Список слов </a>
+  <a href='#'>Тренировка </a>
+  <a href='#'>Список по теме </a>
+
+  <h1>Заголовок Сайта </h1>
+  </>
+  );
+}
+
+  function Main() {
+    return (
+      <>
+        <h3>Список слов</h3>
+        <WordList/>
+        <Card {...data[1]}/>
+      </>
+    );
+    }
+
+function Footer() {
+  return (
+  <>
+  <footer>
+    <h2>Это футер</h2>
+    </footer>
+  </>
+  );
+  }
+
+function Card({word,transcription,translation,topic, }) {
+  let isFront = false;
+  if (isFront){
+    return <div>{word}</div>;
+  } else {
+    return <div>{translation}  {transcription}</div> ;
+  }
+  }
+function Word ({ word, transcription, translation, topic, isEditable}){
+    return (
+      <tr>
+          <td>{isEditable ? <input  difaultvalue={word}/> : word}</td>
+          <td>{isEditable ? <input difaulvalue={translation}/> : translation}</td>
+          <td>{isEditable ? <input difaulvalue={transcription}/> : transcription}</td>
+          <td>{topic}</td>
+          <td>
+            {isEditable ? <button>Seve</button> : null}
+            <button>Delite</button> 
+            <button>Edit</button>
+          </td>
+        </tr>
+    );
+  }
+  
+
+function WordList() {
+  const wordList = data.map((item) => {
+  return <Word key={item.word} {...item} />;
+}
+  )
+
+  return (
+    <table border="2">
+      <thead>
+        <tr>
+          <td>слово</td>
+          <td>перевод</td>
+          <td>транскрипция</td>
+          <td>Тема</td>
+          <td>Кнопки</td>
+        </tr>
+      </thead>
+      <tbody>{wordList}</tbody>
+    </table>
+  );}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Heder/>
+    <Main/>
+    <Footer/>
+  </>
   );
 }
 
---
+++ src/word/data.js
@@ -0,0 +1,36 @@
+export default [
+    {
+      word: 'Sun',
+      transcription:'[sʌn]',
+      translation:'Солнце',
+      topic:'погода',
+      isEditable:true
+    },
+    {
+        word: 'zebra',
+        transcription:'[ˈziː.brə]',
+        translation:'зебра',
+        topic:'животное',
+        isEditable:false
+    },
+    {
+        word: 'car',
+        transcription:'[kɑːr]',
+        translation:'машина',
+        topic:'техника'
+    },
+    {
+        word: 'rainbow ',
+        transcription:'[ˈreɪn.bəʊ]',
+        translation:'радуга',
+        topic:'погода'
+    },
+    {
+        word: 'rainbow ',
+        transcription:'[ˈreɪn.bəʊ]',
+        translation:'радуга',
+        topic:'погода',
+        isEditable:true
+    }
+  ]
+  
