7949a70298612fd801f57a73b23a0c4b52e7933e
NL <Lapina.n.b@mail.ru>
2022-01-20 04:54:32 +1000

    Error

--- src/components/NoMatch.jsx
+++ src/components/NoMatch.jsx
@@ -1,10 +1,14 @@
-import React from "react"
+import React from "react";
+import style from './NoMatch.module.css'
+
+const {styles} = style;
 
 export default function NoMatch(){
     return(
-        <>
-        <img src="https://avatars.mds.yandex.net/get-zen_doc/230574/pub_5ae8e0be1410c3259ffe99e0_5ae8e7244826773d1f54e06f/scale_1200" alt="EROR 404"/>
-        </>
+        <div >
+        <img  className={styles} src="https://avatars.mds.yandex.net/get-zen_doc/230574/pub_5ae8e0be1410c3259ffe99e0_5ae8e7244826773d1f54e06f/scale_1200" 
+        alt="EROR 404"/>
+        </div>
     )
 }
 
--- src/components/heder.jsx
+++ src/components/heder.jsx
@@ -6,22 +6,26 @@ import {
   Link
 } from "react-router-dom";
 import WordList from "./wordList";
-import Card from "./card";
+import CardList from "./cardList";
+import data from "./data";
+import NoMatch from "./NoMatch";
 
 function Heder() {
     return (
     <>
     <Router>
     <Link to= "/WordList">Список слов</Link>
-    <Link to= "/Card">Тренировка </Link>
+    <Link to= "/CardList">Тренировка </Link>
     <Link to='#'>Список по теме </Link>
+  
   
 <h1>Заголовок Сайта </h1>
 
         <Routes>
           <Route exact path="/wordList" element={<WordList />} />
-          <Route exact path="/Card" element={<Card  />} />
+          <Route exact path="/CardList" element={<CardList data={data} />} />
 
+          <Route path="*" element={<NoMatch />} />
         </Routes>
         
     </Router>
